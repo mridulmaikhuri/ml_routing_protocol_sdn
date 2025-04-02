d0b9f221a4f36a9e189efbad8a31ace5708b4e12
mridulmaikhuri <mridulmaikhuri1234@gmail.com>
2025-04-02 10:56:32 +0530

    update topology.py

--- topology.py
+++ topology.py
@@ -1,25 +1,46 @@
+from mininet.net import Mininet
 from mininet.topo import Topo
+from mininet.node import Controller, Node
+from mininet.cli import CLI
+from mininet.log import setLogLevel
 
-class topology(Topo):
+class MyTopo(Topo):
     def build(self):
-        # Add switch
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
-        
-        # Add Hosts
-        h1 = self.addHost('h1', ip='10.0.1.1/24')
-        h2 = self.addHost('h2', ip='10.0.1.2/24')
-        h3 = self.addHost('h3', ip='10.0.2.1/24')
-        h4 = self.addHost('h4', ip='10.0.2.2/24')
+        # Create a router (Linux host acting as a router)
+        router = self.addNode('r1', ip='192.168.1.1/24')
 
-        # Add Links
-        self.addLink(s1, s2)
-        self.addLink(s1, s3)
-        
-        self.addLink(s2, h1)
-        self.addLink(s2, h2)
-        self.addLink(s3, h3)
-        self.addLink(s3, h4)
+        # Create two switches for two subnets
+        switch1 = self.addSwitch('s1')
+        switch2 = self.addSwitch('s2')
 
-topos = {'myTopo': topology}
+        # Add hosts in different subnets
+        h1 = self.addHost('h1', ip='192.168.1.2/24', defaultRoute='via 192.168.1.1')
+        h2 = self.addHost('h2', ip='192.168.1.3/24', defaultRoute='via 192.168.1.1')
+        h3 = self.addHost('h3', ip='10.0.0.2/24', defaultRoute='via 10.0.0.1')
+        h4 = self.addHost('h4', ip='10.0.0.3/24', defaultRoute='via 10.0.0.1')
+
+        # Connect hosts to respective switches
+        self.addLink(h1, switch1)
+        self.addLink(h2, switch1)
+        self.addLink(h3, switch2)
+        self.addLink(h4, switch2)
+
+        # Connect router to both switches
+        self.addLink(router, switch1, intfName1='r1-eth1', params1={'ip': '192.168.1.1/24'})
+        self.addLink(router, switch2, intfName1='r1-eth2', params1={'ip': '10.0.0.1/24'})
+
+def run():
+    net = Mininet(topo=MyTopo(), controller=Controller)
+    net.start()
+
+    router = net.get('r1')
+
+    # Enable IP forwarding on the router
+    router.cmd('sysctl -w net.ipv4.ip_forward=1')
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    run()
