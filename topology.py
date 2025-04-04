c45c4620f2108d37cc3a477240874e4f9eb8fa36
mridulmaikhuri <mridulmaikhuri1234@gmail.com>
2025-04-04 13:56:17 +0530

    commit

--- topology.py
+++ topology.py
@@ -8,14 +8,14 @@ from mininet.log import setLogLevel
 def topology():
     # Initialize Mininet
     net = Mininet(topo=None, build=False, ipBase='10.0.0.0/8')
-    
+
     # Add controller
     print('*** Adding controller')
     c0 = net.addController(name='c0', controller=Controller, protocol='tcp', port=6633)
-    
+
     # Define number of routers and switches
     num = 5
-    
+
     # Add routers
     routers = []
     print('*** Adding routers')
@@ -24,7 +24,7 @@ def topology():
         router = net.addHost(router_name, cls=Node, ip='10.0.{0}.1/24'.format(i+1))
         router.cmd('sysctl -w net.ipv4.ip_forward=1')       
         routers.append(router)
-    
+
     # Add switches
     switches = []
     print('*** Adding switches')
@@ -32,9 +32,9 @@ def topology():
         switch_name = 's{0}'.format(i+1)
         switch = net.addSwitch(switch_name, cls=OVSKernelSwitch)
         switches.append(switch)
-    
+
     # Add hosts 
-    print('*** Adding hosts')
+    print('** Adding hosts')
     hosts = []
     for i in range(num):
         for j in range(2):
@@ -45,25 +45,26 @@ def topology():
                              defaultRoute='via 10.0.{0}.1'.format(i+1))
             hosts.append(host)
             net.addLink(host, switches[i])
-    
+
     # Connect switches to their respective routers
-    print('*** Creating links')
+    print('*** Creating links between switches and routers')
     for i in range(num):
         net.addLink(switches[i], routers[i], 
                    intfName2='r{0}-eth0'.format(i+1), 
                    params2={'ip': '10.0.{0}.1/24'.format(i+1)})
-    
+
     # Create mesh topology by connecting all routers to each other
+    print('*** Creating mesh links between routers')
     link_count = 0
     for i in range(num):
         for j in range(i+1, num):
             link_count += 1
             subnet = 10 + link_count 
-            
+
             # Add link between routers
             intfName1 = 'r{0}-eth{1}'.format(i+1, j+1)
             intfName2 = 'r{0}-eth{1}'.format(j+1, i+1)
-            
+
             net.addLink(
                 routers[i], 
                 routers[j],
@@ -72,21 +73,29 @@ def topology():
                 params1={'ip': '10.0.{0}.{1}/24'.format(subnet, i+1)},
                 params2={'ip': '10.0.{0}.{1}/24'.format(subnet, j+1)}
             )
-            
+
             # Add routes
             routers[i].cmd('ip route add 10.0.{0}.0/24 via 10.0.{1}.{2}'.format(j+1, subnet, j+1))
             routers[j].cmd('ip route add 10.0.{0}.0/24 via 10.0.{1}.{2}'.format(i+1, subnet, i+1))
-    
+
     # Start network
     print('*** Starting network')
     net.build()
-    
+
     # Start controller
     c0.start()
-    
+
     # Start switches
     for switch in switches:
         switch.start([c0])
+
+    # Run CLI
+    print('*** Running CLI')
+    CLI(net)
+
+    # Stop network
+    print('*** Stopping network')
+    net.stop()
 
 if __name__ == '__main__':
     setLogLevel('info')  
